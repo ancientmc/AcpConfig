139a0612bf27ac0901ced4f150ab87be35aec30c
moist-mason <67809772+moist-mason@users.noreply.github.com>
2026-03-30 19:29:36 -0500

    - rd-161348

---
+++ data/versions/preclassic/rd-161348/run/src/main/java/acp/client/Start.java
@@ -0,0 +1,10 @@
+package acp.client;
+
+import com.mojang.minecraft.RubyDung;
+import org.lwjgl.LWJGLException;
+
+public class Start {
+    public static void main(String[] args) throws LWJGLException {
+        RubyDung.main(args);
+    }
+}
